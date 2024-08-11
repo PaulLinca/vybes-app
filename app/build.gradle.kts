7ea68b9f56ccffd76f0ad8f3ac7a883ff03f2c4d
Linca, Paul <paul.linca@sky.uk>
2024-08-11 12:05:39 +0100

    VYBES-APP | Add proper spalsh screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,8 @@ dependencies {
 
     implementation("androidx.compose.material:material:1.6.8")
 
+    implementation(libs.core.splashscreen)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/example/vybes/MainActivity.kt
+++ app/src/main/java/com/example/vybes/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.vybes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -19,6 +20,9 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContent {
             VybesTheme {
                 val navController = rememberNavController()
