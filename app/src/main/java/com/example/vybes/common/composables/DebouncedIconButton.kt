9606ff4b379950fd4b58a1e1017c6ce7833ca5c1
Linca, Paul <paul.linca@sky.uk>
2025-06-20 20:07:22 +0200

    VYBES-APP | Better DebouncedImageButton impl

--- app/src/main/java/com/example/vybes/common/composables/DebouncedIconButton.kt
+++ app/src/main/java/com/example/vybes/common/composables/DebouncedIconButton.kt
@@ -12,12 +12,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.example.vybes.R
-import com.example.vybes.common.theme.IconColor
 import com.example.vybes.common.theme.SubtleBorderColor
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -31,31 +30,15 @@ fun DebouncedIconButton(
     contentDescription: String,
     modifier: Modifier = Modifier
 ) {
-    var isEnabled by remember { mutableStateOf(true) }
-
-    IconButton(
-        onClick = {
-            if (isEnabled) {
-                isEnabled = false
-
-                onClick.invoke()
-
-                CoroutineScope(Dispatchers.Main).launch {
-                    delay(300)
-                    isEnabled = true
-                }
-            }
-        },
-        enabled = isEnabled,
-        modifier = modifier
-    ) {
-        Image(
-            painter = painterResource(id = iconResId),
-            contentDescription = contentDescription,
-            colorFilter = ColorFilter.tint(IconColor),
-            modifier = Modifier.fillMaxSize()
+    DebouncedImageButton(
+        onClick = onClick,
+        contentDescription = contentDescription,
+        modifier = modifier,
+        painter = rememberAsyncImagePainter(
+            model = painterResource(id = iconResId),
+            error = painterResource(id = R.drawable.user)
         )
-    }
+    )
 }
 
 @Composable
@@ -64,6 +47,24 @@ fun DebouncedImageButton(
     pictureUrl: String?,
     contentDescription: String,
     modifier: Modifier = Modifier
+) {
+    DebouncedImageButton(
+        onClick = onClick,
+        contentDescription = contentDescription,
+        modifier = modifier,
+        painter = rememberAsyncImagePainter(
+            model = pictureUrl,
+            error = painterResource(id = R.drawable.user)
+        )
+    )
+}
+
+@Composable
+private fun DebouncedImageButton(
+    onClick: () -> Unit,
+    contentDescription: String,
+    modifier: Modifier = Modifier,
+    painter: Painter
 ) {
     var isEnabled by remember { mutableStateOf(true) }
 
@@ -84,10 +85,7 @@ fun DebouncedImageButton(
         modifier = modifier
     ) {
         Image(
-            painter = rememberAsyncImagePainter(
-                model = pictureUrl,
-                error = painterResource(id = R.drawable.user)
-            ),
+            painter = painter,
             contentDescription = contentDescription,
             modifier = Modifier
                 .fillMaxSize()
